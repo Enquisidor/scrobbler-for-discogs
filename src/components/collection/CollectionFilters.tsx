22e1d9bcc5d860fb834d38f878951706527a8be0
Alex Weinstein <alexrweinstein@gmail.com>
2026-01-12 16:21:19 -0500

    Import edit in CollectionFilters

--- src/components/collection/CollectionFilters.tsx
+++ src/components/collection/CollectionFilters.tsx
@@ -1,7 +1,7 @@
 
 
 import React from 'react';
-import { SortOption } from 'scrobbler-for-discogs-libs/types';
+import { SortOption } from 'scrobbler-for-discogs-libs';
 
 interface CollectionFiltersProps {
   searchTerm: string;
