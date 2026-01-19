9d71b9fc4299abad04d5bc4171c0680647e351c6
Enquisidor <alex.weinstein@improving.com>
2026-01-19 17:13:45 -0500

    fixing tsc errors

---
+++ libs/src/index.ts
@@ -0,0 +1,33 @@
+// Types
+export * from './types';
+
+// Theme
+export * from './theme';
+
+// Strings
+export * from './strings';
+
+// Styles
+export * from './styles/header';
+export * from './styles/mainScreen';
+export * from './styles/settings';
+export * from './styles/collection';
+export * from './styles/queue';
+export * from './styles/misc';
+
+// Utils
+export * from './utils/formattingUtils';
+export * from './utils/collectionUtils';
+export * from './utils/collectionSyncUtils';
+export * from './utils/queueUtils';
+export * from './utils/sortCollection';
+export * from './utils/fuzzyUtils';
+export * from './utils/timeOffsetUtils';
+export * from './utils/trackGroupUtils';
+
+// Services
+export * from './services/appleMusic/appleMusicService';
+export * from './services/musicbrainz/musicbrainzService';
+
+// Adapters
+export * from './adapters';
